d116e260d093be4c6cf066b47e0d6d6b2634e642
TurtleDynasty <TurtleDynasty@users.noreply.github.com>
2016-02-18 23:25:20 -0700

    A few more bugs fixed in driver

--- stringcentral.c
+++ stringcentral.c
@@ -8,9 +8,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// definition of ptr
-
-
 // Will create a string where each of the characters is doubled so that the result string is twice the size of the input string
 // Make sure to provide enough space when calling malloc().
 // For an example string What???, the result string will be WWhhaatt??????
@@ -64,29 +61,27 @@ char* char_shift(char* str){
 
 char* char_flip(char* str){
     while (*str != '\0'){
-        if (*str >= 'A' && str <= 'Z')                                          // Check for uppercase
+        if (*str >= 'A' && *str <= 'Z') // Check for uppercase
         *str = *str + 32;
-        if(*str >= 'a' && str <= 'b')
+        if(*str >= 'a' && *str <= 'z')
         *str = *str - 32;
         str++;
         }
     return str;
 }
 
-int string_central (char*(*ptr[])(char*), char* str){
-    while (*ptr != '\0'){
-        printf("%s", *ptr(str));
+char* string_central (char*(*ptr[])(char*), char* str){
+    while (ptr != '\0'){
+        printf("%s", ptr(str));
         ptr++;
-    return 0;
+    return str;
 }
 
 int main(int argc, char* argv[])
 {
-    //char* ptr = (char*)malloc(sizeof(80));
-    //char* idk = "hello world";
-    //printf("%s", char_double(idk));
-    //return 0;
+    char* ptr = (char*)malloc(sizeof(80));
     char* str = argv[1];
+    // we want an array of pointers to process functions
     char*(*ptr[])(char*) = {*char_nochange, NULL};
     string_central(ptr, str);
     return 0;
